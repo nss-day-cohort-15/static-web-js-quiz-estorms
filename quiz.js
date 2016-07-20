561000776c356a8f25ab38335926c430e2bd03b5
Elizabeth Sanger <libsang@gmail.com>
2016-07-20 09:14:46 -0500

    Ready for review

--- quiz.js
+++ quiz.js
@@ -9,24 +9,13 @@ var tree = {}
 
 var char = document.getElementById('character');
 char.addEventListener('input', function(event) {
-   tree.species = char.value;
+ tree.species = char.value;
 })
 
 var num = document.getElementById('height');
 num.addEventListener('input', function(event){
     tree.height = Number(num.value);
 })
-
-//Initialize function on button click, as long as both fields are populated
-
-// var growButt = document.getElementById('growButton');
-// growButt.addEventListener('click', function(event) {
-//     if (num.value != '' && char.value != '') {
-//         growTree(tree);
-//     }
-//     else {
-//       alert('Aw, shucks. You need a value in both fields.');
-//     }})
 
 
 var growButt = document.getElementById('growButton');
@@ -50,38 +39,39 @@ growButt.addEventListener('click', function(event) {
 //Initialize function on keypress, only while pointer remains in one of the two input fields and both fields are populated
 
 char.addEventListener('keypress', function(event) {
-   if (event.keyCode === 13 && char.value != '' && num.value !='') {
-        growTree(tree);
+ if (event.keyCode === 13 && char.value !== '' && num.value !=='') {
+    growTree(tree);
 
-    }
-    else if (event.keyCode === 13) {
-    alert('Aw, shucks. You need a value in both fields.');
-    }})
+}
+else if (event.keyCode === 13) {
+    alert('You need a value in both fields. How about following the instructions next time.');
+}})
 
 
 num.addEventListener('keypress', function(event) {
-    if (event.keyCode === 13 && char.value != '' && num.value !='') {
+    if (event.keyCode === 13 && char.value !== '' && num.value !=='') {
         growTree(tree);
 
     }
     else if (event.keyCode === 13) {
-    alert('Aw, shucks. You need a value in both fields.');
+        alert('You need a value in both fields. How about following the instructions next time.');
     }
+
 })
 
 
 //Grow tree
- function growTree(arg) {
+function growTree(arg) {
   var space = ' ';
-    for (var i = 1; i <= arg.height; i++) {
+  for (var i = 1; i <= arg.height; i++) {
     console.log(space.repeat(arg.height - i) + arg.species.repeat(i * 2 - 1));
- }
- }
+}
+}
 
 
 
-// Add remaining conditions (if height = NAN, char > 1)
-// Clean up event listeners
+
+
 
 
 
